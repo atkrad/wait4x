3fa94aff9edc04daf9d59ecb36900cb4a2d892f8
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2019-11-10 14:15:29 +0330

    Update tcp command

--- cmd/tcp.go
+++ cmd/tcp.go
@@ -1,27 +1,40 @@
 package cmd
 
 import (
-	"fmt"
+	"errors"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"net"
 	"os"
 	"time"
 )
 
-var host string
-var port string
-
 var tcpCmd = &cobra.Command{
-	Use:   "tcp",
+	Use:   "tcp ADDRESS",
 	Short: "Check TCP connection.",
-	Long:  "",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("ADDRESS is required argument for the tcp command")
+		}
+
+		return nil
+	},
+	Example: `
+  # If you want checking just tcp connection 
+  wait4x tcp 127.0.0.1:9090
+`,
 	Run: func(cmd *cobra.Command, args []string) {
+		timeout, _ := cmd.Flags().GetDuration("timeout")
+
 		var i = 1
 		for i <= RetryCount {
-			fmt.Print(".")
+			log.Info("Checking tcp connection")
 
-			_, err := net.Dial("tcp", host+":"+port)
+			d := net.Dialer{Timeout: timeout}
+			_, err := d.Dial("tcp", args[0])
 			if err != nil {
+				log.Debug(err)
+
 				time.Sleep(Sleep)
 				i += 1
 				continue
@@ -36,6 +49,5 @@ var tcpCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(tcpCmd)
-	tcpCmd.Flags().StringVar(&host, "host", "127.0.0.1", "TCP host.")
-	tcpCmd.Flags().StringVarP(&port, "port", "p", "", "TCP port.")
+	tcpCmd.Flags().Duration("timeout", time.Second*10, "Timeout is the maximum amount of time a dial will wait for a connect to complete.")
 }
