99f43c372016fbd8a3cd1457db45c37f60987b8e
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2020-02-09 09:43:21 +0330

    Added Redis Feature

---
+++ cmd/redis.go
@@ -0,0 +1,121 @@
+package cmd
+
+import (
+	"errors"
+	"github.com/go-redis/redis/v7"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var redisCmd = &cobra.Command{
+	Use:   "redis ADDRESS",
+	Short: "Check Redis connection or key existence.",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("ADDRESS is required argument for the redis command")
+		}
+
+		return nil
+	},
+	Example: `
+  # If you want checking just redis connection 
+  wait4x redis 127.0.0.1:6379
+  
+  # If you want checking redis connection and specify db or password
+  wait4x redis 127.0.0.1:6379 --db 1 --password secret
+  
+  # If you want checking redis connection and key exists or not 
+  wait4x redis 127.0.0.1:6379 --expect-key FOO
+  
+  # Checking redis connection and value expectation of the key 
+  wait4x redis 127.0.0.1:6379 --expect-key FOO=bar
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		timeout, _ := cmd.Flags().GetDuration("timeout")
+		password, _ := cmd.Flags().GetString("password")
+		db, _ := cmd.Flags().GetInt("db")
+		expectKey, _ := cmd.Flags().GetString("expect-key")
+
+		var i = 1
+		for i <= RetryCount {
+			log.Info("Checking redis connection")
+
+			client := redis.NewClient(&redis.Options{
+				Addr:        args[0],
+				Password:    password,
+				DB:          db,
+				DialTimeout: timeout,
+			})
+
+			// Check Redis connection
+			_, err := client.Ping().Result()
+			if err != nil {
+				log.Debug(err)
+
+				time.Sleep(Sleep)
+				i += 1
+				continue
+			} else {
+				// It can connect to Redis successfully
+				splittedKey := strings.Split(expectKey, "=")
+				keyHasValue := len(splittedKey) == 2
+
+				//client.Info().
+				val, err := client.Get(splittedKey[0]).Result()
+				if err == redis.Nil {
+					// Redis key does not exist.
+					log.WithFields(log.Fields{
+						"key": splittedKey[0],
+					}).Info("Key does not exist.")
+
+					time.Sleep(Sleep)
+					i += 1
+					continue
+				} else if err != nil {
+					// Error occurred on get Redis key
+					log.Debug(err)
+
+					time.Sleep(Sleep)
+					i += 1
+					continue
+				} else {
+					// The Redis key exists.
+					if !keyHasValue {
+						os.Exit(0)
+					}
+
+					// When the user expect a key with value
+					matched, _ := regexp.MatchString(splittedKey[1], val)
+					if matched {
+						os.Exit(0)
+					} else {
+						log.WithFields(log.Fields{
+							"key": splittedKey[0],
+							"actual": val,
+							"expect": splittedKey[1],
+						}).Info("Checking value expectation of the key")
+
+						time.Sleep(Sleep)
+						i += 1
+						continue
+					}
+				}
+			}
+		}
+
+		os.Exit(1)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(redisCmd)
+	redisCmd.Flags().Duration("timeout", time.Second*5, "Dial timeout for establishing new connections.")
+	redisCmd.Flags().String("password", "", "Optional password. Must match the password specified in the requirepass server configuration option.")
+	redisCmd.Flags().String("expect-key", "", "Optional password. Must match the password specified in the requirepass server configuration option.")
+	redisCmd.Flags().String("expect-info", "", "Optional password. Must match the password specified in the requirepass server configuration option.")
+	redisCmd.Flags().Int("db", 0, "Database to be selected after connecting to the server.")
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/atkrad/wait4x
 go 1.13
 
 require (
+	github.com/go-redis/redis/v7 v7.0.0-beta.6
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
 )
