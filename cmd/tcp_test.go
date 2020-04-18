ecff82bf9c74a428cb92352fad2d6eb4311114a2
Mohammad Abdolirad <m.abdolirad@gmail.com>
2020-04-18 15:08:44 +0430

    Added new test case
    Signed-off-by: Mohammad Abdolirad <m.abdolirad@gmail.com>

--- cmd/tcp_test.go
+++ cmd/tcp_test.go
@@ -16,6 +16,15 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+func TestTcpCommandInvalidArgument(t *testing.T) {
+	wait4xCommand := NewWait4X()
+	wait4xCommand.AddCommand(NewTcpCommand())
+
+	_, err := test.ExecuteCommand(wait4xCommand, "tcp")
+
+	assert.Equal(t, "ADDRESS is required argument for the tcp command", err.Error())
+}
+
 func TestTcpConnectionSuccess(t *testing.T) {
 	wait4xCommand := NewWait4X()
 	wait4xCommand.AddCommand(NewTcpCommand())
