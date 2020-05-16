f14820f50e48abb173bf211abac4b079ffd215e7
Mohammad Abdolirad <m.abdolirad@gmail.com>
2020-05-16 11:43:13 +0430

    Add log level flag
    Signed-off-by: Mohammad Abdolirad <m.abdolirad@gmail.com>

--- cmd/wait4x.go
+++ cmd/wait4x.go
@@ -2,10 +2,12 @@ package cmd
 
 import (
 	"errors"
-	errs "github.com/atkrad/wait4x/internal/errors"
-	"github.com/spf13/cobra"
 	"os"
 	"time"
+
+	errs "github.com/atkrad/wait4x/internal/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -13,6 +15,8 @@ var (
 	Interval time.Duration
 	// Timeout represents the maximum amount of time that Wait4X will wait for a checking operation
 	Timeout time.Duration
+	// LogLevel represents logging level e.g. info, warn, error, debug
+	LogLevel string
 )
 
 // NewWait4X creates the root command
@@ -21,14 +25,25 @@ func NewWait4X() *cobra.Command {
 		Use:   "wait4x",
 		Short: "Wait4X allows waiting for a port or a service to enter into specify state",
 		Long:  `Wait4X allows waiting for a port to enter into specify state or waiting for a service e.g. redis, mysql, postgres, ... to enter inter ready state`,
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			// Prevent showing usage when subcommand return error.
 			cmd.SilenceUsage = true
+
+			lvl, err := logrus.ParseLevel(LogLevel)
+			if err != nil {
+				return err
+			}
+
+			logrus.SetOutput(os.Stdout)
+			logrus.SetLevel(lvl)
+
+			return nil
 		},
 	}
 
 	wait4x.PersistentFlags().DurationVarP(&Interval, "interval", "i", 1*time.Second, "Interval time between each loop.")
 	wait4x.PersistentFlags().DurationVarP(&Timeout, "timeout", "t", 10*time.Second, "Timeout is the maximum amount of time that Wait4X will wait for a checking operation.")
+	wait4x.PersistentFlags().StringVarP(&LogLevel, "log-level", "l", logrus.InfoLevel.String(), "Set the logging level (\"debug\"|\"info\"|\"warn\"|\"error\"|\"fatal\")")
 
 	return wait4x
 }
--- main.go
+++ main.go
@@ -2,13 +2,7 @@ package main
 
 import (
 	"github.com/atkrad/wait4x/cmd"
-	log "github.com/sirupsen/logrus"
-	"os"
 )
-
-func init() {
-	log.SetOutput(os.Stdout)
-}
 
 func main() {
 	cmd.Execute()
