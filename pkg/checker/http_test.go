d8ef2c2035be92f82ce3acf5e4b0331b4236d306
Mohammad Abdolirad <m.abdolirad@gmail.com>
2021-03-05 23:55:52 +0330

    Add new HTTP unit tests
    Signed-off-by: Mohammad Abdolirad <m.abdolirad@gmail.com>

--- pkg/checker/http_test.go
+++ pkg/checker/http_test.go
@@ -17,6 +17,29 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+func TestHttpInvalidAddress(t *testing.T) {
+	logger, _ := log.NewLogrus(logrus.DebugLevel.String(), ioutil.Discard)
+
+	hc := NewHTTP("http://not-exists.tld", 0, "", time.Second*5)
+	hc.SetLogger(logger)
+
+	assert.Equal(t, false, hc.Check())
+}
+
+func TestHttpValidAddress(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer ts.Close()
+
+	logger, _ := log.NewLogrus(logrus.DebugLevel.String(), ioutil.Discard)
+
+	hc := NewHTTP(ts.URL, 0, "", time.Second*5)
+	hc.SetLogger(logger)
+
+	assert.Equal(t, true, hc.Check())
+}
+
 func TestHttpInvalidStatusCode(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -40,6 +63,36 @@ func TestHttpValidStatusCode(t *testing.T) {
 	logger, _ := log.NewLogrus(logrus.DebugLevel.String(), ioutil.Discard)
 
 	hc := NewHTTP(ts.URL, http.StatusOK, "", time.Second*5)
+	hc.SetLogger(logger)
+
+	assert.Equal(t, true, hc.Check())
+}
+
+func TestHttpInvalidBody(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Wait4X"))
+	}))
+	defer ts.Close()
+
+	logger, _ := log.NewLogrus(logrus.DebugLevel.String(), ioutil.Discard)
+
+	hc := NewHTTP(ts.URL, 0, "FooBar", time.Second*5)
+	hc.SetLogger(logger)
+
+	assert.Equal(t, false, hc.Check())
+}
+
+func TestHttpValidBody(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Wait4X is the best CLI tools."))
+	}))
+	defer ts.Close()
+
+	logger, _ := log.NewLogrus(logrus.DebugLevel.String(), ioutil.Discard)
+
+	hc := NewHTTP(ts.URL, 0, "Wait4X.+best.+tools", time.Second*5)
 	hc.SetLogger(logger)
 
 	assert.Equal(t, true, hc.Check())
