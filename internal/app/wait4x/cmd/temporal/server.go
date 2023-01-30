c726de2cdde66d63bf220df4cc344f4bc8dda789
Mohammad Abdolirad <m.abdolirad@gmail.com>
2023-01-30 11:58:03 +0100

    Change the copyright header

--- internal/app/wait4x/cmd/temporal/server.go
+++ internal/app/wait4x/cmd/temporal/server.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Wait4X Authors
+// Copyright 2023 The Wait4X Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
