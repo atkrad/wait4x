57e218ea0df3a5dea952754ae15509e656c4e790
Vinicios Wentz <vinicios@wentz.io>
2023-06-02 19:23:58 -0300

    cassandra check implementation
    Signed-off-by: Vinicios Wentz <vinicios@wentz.io>

--- checker/cassandra/cassandra.go
+++ checker/cassandra/cassandra.go
@@ -4,7 +4,12 @@ import (
 	"context"
 	"errors"
 
+	"github.com/gocql/gocql"
 	"wait4x.dev/v2/checker"
+)
+
+var (
+	ErrBadConnection = errors.New("bad connection")
 )
 
 type Cassandra struct {
@@ -18,9 +23,45 @@ func New(dnss []string) checker.Checker {
 }
 
 func (c *Cassandra) Identity() (string, error) {
-	return "", errors.New("not impl yet")
+	return "cassandra", nil
 }
 
 func (c *Cassandra) Check(ctx context.Context) error {
-	return errors.New("not impl yet")
+	cluster := gocql.NewCluster(c.dnss...)
+	cluster.Keyspace = "wait4x"
+
+	session, err := cluster.CreateSession()
+	defer session.Close()
+	if err != nil {
+		return err
+	}
+
+	iter := session.Query("select cql_version from system.local").
+		WithContext(ctx).
+		Iter()
+	defer iter.Close()
+
+	rows, err := iter.RowData()
+	if err != nil {
+		return err
+	}
+
+	if len(rows.Values) != 1 {
+		return ErrBadConnection
+	}
+
+	if ok := iter.Scan(rows.Values...); !ok {
+		return ErrBadConnection
+	}
+
+	values, ok := rows.Values[0].(*string)
+	if !ok {
+		return ErrBadConnection
+	}
+
+	if len(*values) < 1 {
+		return ErrBadConnection
+	}
+
+	return nil
 }
